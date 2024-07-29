dfef278149bb8832645baa5aebd8ff4b4c1b6877
AdamDavis08 <hristijangajdov@gmail.com>
2024-07-29 21:38:48 +0200

    Speedster Added

---
+++ MainMenuAnimation.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimation : MonoBehaviour
+{
+    [SerializeField] private float planeSpeed = 1f;
+    [SerializeField] private float earthSpeed = 1f;
+
+    [SerializeField] private Transform planePivot;
+    [SerializeField] private Transform planeEndPoint;
+    [SerializeField] private Transform earth;
+    
+    private void Update()
+    {
+        float singleStep = planeSpeed * Mathf.Deg2Rad * Time.deltaTime;
+        
+        earth.Rotate(0f, earthSpeed * Time.deltaTime, 0f);
+        
+        if (Vector3.Distance(planePivot.GetChild(0).position, planeEndPoint.position) > 0.1f)
+            planePivot.Rotate(0f, planeSpeed * Time.deltaTime, 0f);
+    }
+}
